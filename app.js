ab6f91df2a5ba4a6d58bec38c71b30e43ba64949
rayannesilva <56279350+rayannesilva@users.noreply.github.com>
2020-10-02 17:34:26 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(over('X-HTTP-Method-Override'))
 app.use(over('_method', {methods:['GET', 'POST', 'PUT', 'DELETE']}))
 const moeda = require('./moeda')
-//app.get('/',  moeda.moedac ) //rota teste
 //json estatico de alunos
 app.get('/aluno/todos', (req, res) => {
 	let alunos = [
@@ -17,12 +16,12 @@ app.get('/aluno/todos', (req, res) => {
 		{'nome': 'Lorin'},
 		{'nome': 'Yodinha'}
 	]
-	res.json(alunos)})
+	res.json(alunos)
+})
 app.get('/moeda/:valor_real', (req, res) => {
 		
 		res.json(moeda.moedac)})
 	
  
-app.listen(3000, () =>{//testando
-	console.log('funcionando')
+app.listen(3000, () =>{//feedback
 })
